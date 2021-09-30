9ce14d2e21ce4140834646a6a0317e0ce2729832
FinnJanssensEHB <finn.janssens@student.ehb.be>
2021-09-30 12:17:30 +0200

    Added formEventHandler

--- S1/index.js
+++ S1/index.js
@@ -2,4 +2,11 @@
 
 window.onload = function () {
   console.log("loaded");
+  const submitBtn = document.getElementById("submit");
+  submitBtn.addEventListener("click", formEventListener);
+}
+
+function formEventListener(e) {
+  e.preventDefault();
+  console.log("Submit button was clicked");
 }
