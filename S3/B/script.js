225866f138c6f34bdc8b2138b27350f6c0baa197
FinnJanssensEHB <finn.janssens@student.ehb.be>
2021-10-14 11:34:12 +0200

    S3b: testing basic API call

--- S3/B/script.js
+++ S3/B/script.js
@@ -1,1 +1,17 @@
 "use strict;"
+
+const BASE_URL = "http://www.omdbapi.com/?";
+const API_KEY = "70468ac";
+
+window.onload = function () {
+  let callURL = `${BASE_URL}t=Parasite&y=2019&apikey=${API_KEY}`;
+  getData(callURL).then(d => console.log(d));
+};
+
+async function getData(url) {
+  let response = await fetch(url);
+  if (response.ok) {
+    let data = await response.json();
+    return data;
+  } else console.log("Fetch call returned: " + response.status);
+}
