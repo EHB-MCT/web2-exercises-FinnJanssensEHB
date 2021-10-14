783afae8068677cce76c4b8ee36b40e25e6dbfc1
FinnJanssensEHB <finn.janssens@student.ehb.be>
2021-10-14 11:02:44 +0200

    S3a: added promise to random number guesser

--- S3/index.js
+++ S3/index.js
@@ -1,30 +1,19 @@
 "use strict";
 
-let input, message;
 let rndNumber = Math.floor(Math.random() * 10);
 let output = document.getElementById('message');
 
 
 window.onload = function () {
-  document.getElementById('submitBtn').addEventListener('click', function () {
-    getNumberInput();
+  document.getElementById('submitBtn').addEventListener('click', e => {
+    compareNumbers(document.getElementById("inputField").value);
   });
 }
 
-function getNumberInput() {
-  input = document.getElementById("inputField").value;
-  compareNumbers(input);
-}
-
 function compareNumbers(input) {
-  console.log(input);
-  if (input == rndNumber) {
-    output.innerText = "You guessed the right number!"
-  } else if (input > rndNumber) {
-    output.innerText = "The random number is smaller!"
-  } else if (input < rndNumber) {
-    output.innerText = "The random number is bigger!"
-  } else {
-    output.innerText = "Wrong input"
-  }
+  return new Promise(function (resolve, reject) {
+    if (input == rndNumber) resolve("You guessed the right number!");
+    else if (input > rndNumber) reject("The random number is smaller!");
+    else if (input < rndNumber) reject("The random number is bigger!");
+  });
 }
