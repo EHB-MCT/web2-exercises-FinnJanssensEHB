e85c843c91cdc7a42eba09d079cfa20fa1d55791
FinnJanssensEHB <finn.janssens@student.ehb.be>
2021-10-21 12:37:06 +0200

    S4: sexy inputs done

--- S4/src/index.js
+++ S4/src/index.js
@@ -1,12 +1,48 @@
 import _ from 'lodash';
+import Cleave from 'cleave.js';
 
-function component() {
-  const element = document.createElement('div');
+require('cleave.js/dist/addons/cleave-phone.BE');
 
-  // Lodash, now imported by this script
-  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-
-  return element;
+window.onload = function () {
+  console.log("window loaded");
+  document.getElementById('inputForm').addEventListener('submit', showInfo);
 }
 
-document.body.appendChild(component());
+var cleaveName = new Cleave('#nameInput', {
+  prefix: 'STUDENT-'
+});
+
+var cleaveBirthDate = new Cleave('#birthDateInput', {
+  date: true,
+  delimiter: '-',
+  datePattern: ['d', 'm', 'Y']
+});
+
+var cleaveRRN = new Cleave('#rrnInput', {
+  delimiters: ['.', '.', '-', '.'],
+  blocks: [2, 2, 2, 3, 2]
+});
+
+var cleaveAge = new Cleave('#ageInput', {
+  numeral: true,
+  numeralPositiveOnly: true
+});
+
+
+var cleavePhone = new Cleave('#phoneInput', {
+  phone: true,
+  phoneRegionCode: 'BE'
+});
+
+function showInfo(e) {
+  e.preventDefault();
+  console.log("submit");
+  let jumbo = document.getElementById('infoJumbotron');
+  console.log(jumbo.children);
+  jumbo.children[0].innerHTML = document.getElementById('nameInput').value;
+  jumbo.children[2].innerHTML = `Birth Date: ${document.getElementById('birthDateInput').value}`;
+  jumbo.children[3].innerHTML = `RRN: ${document.getElementById('rrnInput').value}`;
+  jumbo.children[4].innerHTML = `Age: ${document.getElementById('ageInput').value}`;
+  jumbo.children[5].innerHTML = `Phone Number: ${document.getElementById('phoneInput').value}`;
+  jumbo.style.display = "block";
+}
