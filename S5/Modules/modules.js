3113fdb0b5056e951a1bd61acdcf416a188c1348
FinnJanssensEHB <finn.janssens@student.ehb.be>
2021-11-18 10:32:53 +0100

    Changed E2 from CJS to ESM

--- S5/Modules/modules.js
+++ S5/Modules/modules.js
@@ -1,20 +1,26 @@
-var fs = require('fs');
+import * as fs from 'fs/promises';
 
+let result = await fs.readFile("./boardgames.json");
+let boardgames = JSON.parse(result);
+for (let property in boardgames) {
+  console.log(property);
+  await fs.writeFile(`${property}.json`, JSON.stringify(boardgames[property]));
+}
 
-fs.promises.readFile("./boardgames.json")
-  .then(function (result) {
-    let fileContent = JSON.parse(result);
-    for (const [key, value] of Object.entries(fileContent)) {
-      console.log(JSON.stringify(value));
-      fs.promises.writeFile(`./${key}.json`, JSON.stringify(value))
-        .then(function (result) {
-          console.log("" + result);
-        })
-        .catch(function (error) {
-          console.log(error);
-        })
-    }
-  })
-  .catch(function (error) {
-    console.log(error);
-  })
+// fs.promises.readFile("./boardgames.json")
+//   .then(function (result) {
+//     let fileContent = JSON.parse(result);
+//     for (const [key, value] of Object.entries(fileContent)) {
+//       console.log(JSON.stringify(value));
+//       fs.promises.writeFile(`./${key}.json`, JSON.stringify(value))
+//         .then(function (result) {
+//           console.log("" + result);
+//         })
+//         .catch(function (error) {
+//           console.log(error);
+//         })
+//     }
+//   })
+//   .catch(function (error) {
+//     console.log(error);
+//   })
